e8f55cd0023fc8f407a6779f3cd95cf7e9fb3298
AkashAgrawal18 <agrawal.akash0381@gmail.com>
2024-04-28 13:47:01 +0530

    config reset

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3030,
-    proxy: {
-      '/api': {
-        target: 'https://myportfolio-backend-e0rb.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+    proxy:{
+      '/api' : 'https://myportfolio-backend-e0rb.onrender.com'
+    }
       },
-    },
-  },
   plugins: [
     react(),
   ],
